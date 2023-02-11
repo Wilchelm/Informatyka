9bbdf586f6a730c8dbd3e2dda60dd53bee3d5a56
Wilchelm <15729616+Wilchelm@users.noreply.github.com>
2023-02-11 13:47:46 +0100

    DSTA

---
+++ DSTA/funkcja 16.11.2016.R
@@ -0,0 +1,5 @@
+par.lambda = function(x)
+{
+  z = 1/length(x)*sum(x^2)
+  return (z)
+}
---
+++ DSTA/w.test.R
@@ -0,0 +1,7 @@
+function(x,y,alt)
+{ 
+  w=list(statistic, parameter. p.value, alternative, method, data.name)
+  class(w)="htest"
+  z=
+  return z
+}
